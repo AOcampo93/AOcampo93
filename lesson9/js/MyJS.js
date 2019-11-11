9d10b9fcee1977feaeb02a4f9415e4fd6bb278b5
ArturoOcampo93 <arturo.ocampo93@gmail.com>
2019-11-11 10:38:03 -0600

    Lesson 9 - JSON activity

---
+++ lesson9/js/MyJS.js
@@ -0,0 +1,20 @@
+function menu(){
+    var x = document.getElementById('menu');
+    var y = document.getElementById('menutext');
+
+    if (x.style.display === 'block') {
+        document.getElementById("menutext").style.animation = "slideInLeft 1s 1";
+        document.getElementById("menu").style.animation = "Up 1s 1";
+        y.style.display = 'none';
+        var z = setTimeout(close,1000);
+      } else {
+        x.style.display = 'block';
+        y.style.display = 'block';
+        document.getElementById("menu").style.animation = "Down 1s 1";
+        document.getElementById("menutext").style.animation = "slideInLeft 1s 1";
+    }
+    return false;
+}
+function close(){
+  document.getElementById('menu').style.display = "none";
+}
